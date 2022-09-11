fbec2c4cf69e139b9b92faf89cd11944fdd4c5b2
Katherine Moffat <kmof0001@student.monash.edu>
2022-09-11 22:50:30 +1000

    added result page and Post method to form

--- website/views.py
+++ website/views.py
@@ -1,21 +1,31 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 
 views = Blueprint('views', __name__)
 
 
 @views.route('/')
 def home():
-    'this will run when on the / route for website'
+    # this will run when on the / route for website
     return render_template("home.html")
 
 
-@views.route('/tool')
+@views.route('/tool', methods=['GET', 'POST'])
 def tool():
-    'this will run when on the / route for website'
+    # when user hits submit - post request to result page
+    if request.method == 'POST':
+        job_ad = request.form.get('job-ad')
+        # --
+        # do some stuff with the job ad
+        # ---
+        return (redirect(url_for('views.result')))
     return render_template("tool.html")
 
 
 @views.route('/about')
 def about():
-    'this will run when on the / route for website'
     return render_template("about.html", text="passing data")
+
+
+@views.route('/result')
+def result():
+    return render_template("result.html")
